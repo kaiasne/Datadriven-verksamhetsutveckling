6e2b4b3f36032f6905e1e62a43de108a89029ce8
Arvid C <100345778+Kodemen@users.noreply.github.com>
2023-05-03 11:04:42 +0200

    Update NLP.py
    Uppdatera NLP.py med adjektive information

--- NLP.py
+++ NLP.py
@@ -13,18 +13,33 @@ text_data = data['description']
 # Convert text data to lowercase
 text_data = text_data.str.lower()
 
-# Tokenize the text data into individual words
-words = []
-for text in text_data:
-    words.extend(word_tokenize(text))
+# Tokenize each text into individual words
+tokenized_data = [word_tokenize(text.lower()) for text in text_data]
 
-# Remove stop words and junk words
-stop_words = set(stopwords.words('swedish'))
-junk_words = {'.', ',', ':', ';', '?', '!', '(', ')', '[', ']', '{', '}',"'",}
-words = [word for word in words if word not in stop_words and word not in junk_words]
+# Tag the words in each text with their part of speech
+tagged_data = [nltk.pos_tag(tokens) for tokens in tokenized_data]
 
-# Count the number of times each word appears
-word_counts = Counter(words)
+# Extract the adjectives from each text
+adjectives_data = [[word for word, pos in tagged_words if pos.startswith('JJ')] for tagged_words in tagged_data]
 
-# Print the most common words
-print(word_counts.most_common(10))
+# Print the adjectives for each text
+for i, adjectives in enumerate(adjectives_data):
+    print(f'Text {i+1} adjectives: {adjectives}')
+
+# # Tokenize the text data into individual words
+# words = []
+# for text in text_data:
+#     words.extend(word_tokenize(text))
+
+# # Remove stop words and junk words
+# stop_words1 = set(stopwords.words('swedish'))
+# stop_words2 = set(stopwords.words('english'))
+# stop_words = stop_words1 | stop_words2
+# junk_words = {'.', ',', ':', ';', '?', '!', '(', ')', '[', ']', '{', '}',"'",}
+# words = [word for word in words if word not in stop_words and word not in junk_words]
+
+# # Count the number of times each word appears
+# word_counts = Counter(words)
+
+# # Print the most common words
+# print(word_counts.most_common(20))
