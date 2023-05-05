5d86f1bff342e39ea072805e7861749c91c396c9
Havinger <cj.havinger@gmail.com>
2023-05-05 11:45:07 +0200

    Install NLTK

--- NLP analys.py
+++ NLP analys.py
@@ -1,8 +1,10 @@
 import nltk
-nltk.download('all')
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from collections import Counter
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import sent_tokenize
+
+
+
