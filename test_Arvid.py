abf3efc4d7fe32cc80a97876dc1b0256abf0f1ce
Arvid C <100345778+Kodemen@users.noreply.github.com>
2023-05-12 13:20:15 +0200

    Update test_Arvid.py

--- test_Arvid.py
+++ test_Arvid.py
@@ -1,12 +1,24 @@
-import streamlit as st
+# import streamlit as st
 
-st.title("My Streamlit Test App")
+# st.title("My Streamlit Test App")
 
-st.write("Welcome to my test page!")
+# st.write("Welcome to my test page!")
 
-x = st.slider('Select a value:', 0, 100, 50)
-st.write('You selected:', x)
+# x = st.slider('Select a value:', 0, 100, 50)
+# st.write('You selected:', x)
 
-if st.button('Double the value'):
-    x *= 2
-    st.write('New value:', x)
+# if st.button('Double the value'):
+#     x *= 2
+#     st.write('New value:', x)
+
+import pandas as pd
+
+# Read the CSV file into a pandas dataframe
+df = pd.read_csv('TestSys.csv')
+
+# Group the dataframe by name and sum the numbers in the other column
+counts = df.groupby('workplace_address')['number_of_vacancies'].sum()
+
+# Print the counts for each name
+print(counts)
+
