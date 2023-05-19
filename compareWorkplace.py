702364d49517d4d08c1075e3ac8b5e729ff779d5
kaiasne <kaiasne@gmail.com>
2023-05-19 10:39:07 +0200

    Update compareWorkplace.py

--- compareWorkplace.py
+++ compareWorkplace.py
@@ -5,7 +5,7 @@ import pandas as pd
 import altair as alt
 
 # Load data
-df = pd.read_csv('output_merge.csv')
+df = pd.read_csv('2021_2022_Streamlit.csv')
 
 # Convert Headline column to string data type
 df['Headline'] = df['Headline'].astype(str)
