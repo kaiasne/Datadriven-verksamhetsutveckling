243453cfcfec3bb07ce44f0685b181b981133582
Arvid C <100345778+Kodemen@users.noreply.github.com>
2023-05-31 16:37:01 +0200

    Update main_STREAMLIT.py

--- main_STREAMLIT.py
+++ main_STREAMLIT.py
@@ -16,7 +16,7 @@ desired_width = int(desired_height * width_to_height_ratio)
 # Get list of unique workplaces
 workplaces = df['Workplace'].dropna().unique()
 
-st.sidebar.image("sdff.png", caption="Sundsvalls Damfotbollsförening", width=desired_width,)
+st.sidebar.image("sdff.png", caption="Sundsvalls Damfotbollsförening ", width=desired_width,)
 
 # Sidebar for selecting workplaces
 selected_workplaces = st.sidebar.multiselect('Välj komun', workplaces)
@@ -30,30 +30,22 @@ df_workplaces_grouped = df_workplaces.groupby(['MergedColumn', 'Workplace', 'Hea
 # Create line chart with larger tooltip area
 line_chart = alt.Chart(df_workplaces_grouped).mark_line().encode(
     x=alt.X('MergedColumn:O', title='Kvartal'),
-    y=alt.Y('VacancyCount', title='Antal jobb'),
+    y=alt.Y('VacancyCount:Q', title='Antal jobb'),
     color='Workplace',
-    tooltip=[
-        alt.Tooltip('Headline', title='Jobbannonsen'),
-        alt.Tooltip('VacancyCount', title='Antal jobb')
-    ],
-    size=alt.value(2)  # Set tooltip size to 150
 ).properties(
     title='Antal jobb per kvartal',
     width=600,
     height=400
-).configure_mark(
-    opacity=0.7,  # Set line opacity
-    strokeWidth=2  # Set line width
-).configure_axis(
-    labelFontSize=12,  # Set axis label font size
-    titleFontSize=14  # Set axis title font size
-).configure_title(
-    fontSize=16  # Set chart title font size
-).configure_view(
-    strokeWidth=0  # Remove border around chart
-).configure_legend(
-    title=None  # Remove legend title
+) + alt.Chart(df_workplaces_grouped).mark_circle(size=150).encode(
+    x=alt.X('MergedColumn:O', title='Kvartal'),
+    y=alt.Y('VacancyCount:Q', title='Antal jobb'),
+    color='Workplace',
+    tooltip=[
+        alt.Tooltip('Headline:N', title='Jobbannonser'),
+        alt.Tooltip('VacancyCount:Q', title='Antal jobb')
+    ]
 )
+
 with st.sidebar.expander("Aktiva jobbannonser:"):
     # Display the URL code
     st.markdown('[Butiksmedarbetare Sportringen, Sundsvall](https://arbetsformedlingen.se/platsbanken/annonser/27620458)')
